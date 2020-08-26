1fd5c1647ec4621172b3ada1bde0fb876312ed35
Evin Ramadeen <evinramadeen@gmai.com>
2020-08-26 00:54:16 -0400

    Decided to leave the logout button on main categories page only. I included the functionality to show a summary of the category when the sub category is clicked.  Next up is to ensure i can work with files as well as upload them on the firestore website. then work on favorites.

--- lib/information/sub_categories.dart
+++ lib/information/sub_categories.dart
@@ -23,8 +23,9 @@ class _SubCategoriesPageState extends State<SubCategoriesPage> {
   }
 }
 
+//buildbody is used to create the body. at first stream builder takes the snapshot of the data in the database.
+//by using stream builder, it updates the data in real time. may not actually be needed since my data will be static. will review the negatives of using it
 Widget _buildBody(BuildContext context, String mainCat) {
-  //CollectionReference users = Firestore.instance.collection('category').where('main_category', isEqualTo: mainCat);
   return StreamBuilder<QuerySnapshot>(
       stream: Firestore.instance
           .collection('category')
@@ -38,14 +39,18 @@ Widget _buildBody(BuildContext context, String mainCat) {
         } else if (!snapshot.hasData)
           return LinearProgressIndicator(); //shows a progress bar if no data found. Will have to edit this
         else
-          return _buildList(context, snapshot.data.documents);
+          return _buildList(
+              context,
+              snapshot.data
+                  .documents); //calls a function named build list. tried to leave the functions modular.
       });
 }
 
 Widget _buildList(BuildContext context, List<DocumentSnapshot> snapshot) {
   return ListView(
     padding: const EdgeInsets.only(top: 20.0),
-    children: snapshot.map((data) => _buildListItem(context, data)).toList(),
+    children: snapshot.map((data) => _buildListItem(context, data))
+        .toList(), // the to list statement is what turns the data into the list form basically
   );
 }
 
@@ -56,21 +61,33 @@ Widget _buildListItem(BuildContext context, DocumentSnapshot data) {
     key: ValueKey(record.main_category),
     padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
     child: Container(
-      decoration: BoxDecoration(
-        border: Border.all(color: Colors.green),
-        borderRadius: BorderRadius.circular(5.0),
-      ),
-      child: ListTile(
-        title: Text(record.sub_category),
-        onTap: () => print(record.document),
-      ),
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.green),
+          borderRadius: BorderRadius.circular(5.0),
+        ),
+        //expansion tile allows the user to click the sub category and view a summary of which each category is about.
+        child: ExpansionTile(
+          title: Text(record.sub_category,
+            style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),),
+          children: <Widget>[
+            ListTile( //this is where the summary of the particular item will be.
+              title: Text(record.summary),
+              onTap: () => printSum(record.summary.toString()),
+            )
+          ],
+        )
     ),
   );
 }
+//delete this when a proper summary is in place. can put the jump to the document in here if i dont delete it.
+void printSum(String holder) {
+  print(holder + ' was clicked');
+}
 
 class Record {
+  //this class mimics all of the fields in the database. it allows me to reference the different fields.
   final String main_category;
-  final String document;
+  final String document; //this will have to change when i enter the html documents.
   final String sub_category;
   final String summary;
   final DocumentReference reference;
